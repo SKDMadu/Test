07d4c574999c334690aa63a77baf4dac6a5440f5
SKDMadu <38666001+SKDMadu@users.noreply.github.com>
2018-04-24 22:33:03 +0530

    Update New.java

--- javacode/New.java
+++ javacode/New.java
@@ -2,7 +2,7 @@ public class New{
 
     System.out.println("I am hungry");
 	System.out.println("I have coffee");
-	
+	System.out.println("Typ");
 	System.out.println("I am Thirsty")
 	System.out.println("I am soda")
 	System.out.println("Typpp");
